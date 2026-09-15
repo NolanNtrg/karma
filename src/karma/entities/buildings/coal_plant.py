12dccc388a4037ec53b6a1e3af85c182e2c62a37
Enzo Morello <enzomorello.pro@gmail.com>
2026-09-15 14:42:20 +0200

    add: karma system

--- src/karma/entities/buildings/coal_plant.py
+++ src/karma/entities/buildings/coal_plant.py
@@ -15,4 +15,4 @@ class CoalPlant(EnergyProducer):
         productionAmount: int,
         productionInterval: float,
     ) -> None:
-        super().__init__(position, health, energyCost, -1.0, productionAmount, productionInterval)
+        super().__init__(position, health, energyCost, -100.0, productionAmount, productionInterval)
