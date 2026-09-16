5368aa375a27ab72751d768e660b560b0f0ec747
Enzo Morello <enzomorello.pro@gmail.com>
2026-09-16 20:28:47 +0200

    shortcuts F1-F6 pour cheats

---
+++ src/karma/systems/cheats.py
@@ -0,0 +1,57 @@
+import random
+
+import pygame
+
+from karma.entities.buildings.base import Base
+from karma.enums import RessourceType
+from karma.resources.ressourceManager import RessourceManager
+from karma.settings import KARMA_MAX, KARMA_MIN
+from karma.systems.combat import CombatSystem
+from karma.systems.cycle import CycleSystem
+
+SPAWN_WAVE_KEYS: dict[int, int] = {pygame.K_F3: 25}
+
+
+class CheatSystem:
+    # Raccourcis de debug (F1-F7) pour tester rapidement le jeu
+
+    def __init__(self, base: Base, cycle_system: CycleSystem, combat_system: CombatSystem, rm: RessourceManager) -> None:
+        self.base = base
+        self.cycle_system = cycle_system
+        self.combat_system = combat_system
+        self.rm = rm
+        self.godmode: bool = False
+
+    def handleKey(self, key: int) -> None:
+        if key == pygame.K_F1:
+            self.toggleGodmode()
+        elif key == pygame.K_F2:
+            self.skipPhase()
+        elif key in SPAWN_WAVE_KEYS:
+            self.spawnEnemies(SPAWN_WAVE_KEYS[key])
+        elif key == pygame.K_F4:
+            self.maxResources()
+        elif key == pygame.K_F5:
+            self.rm.setStock(RessourceType.Karma, KARMA_MAX)
+        elif key == pygame.K_F6:
+            self.rm.setStock(RessourceType.Karma, KARMA_MIN)
+
+    def toggleGodmode(self) -> None:
+        # Le vaisseau ne prend plus de dégâts
+        self.godmode = not self.godmode
+        self.base.invincible = self.godmode
+
+    def skipPhase(self) -> None:
+        # Passe directement à la phase suivante (jour/nuit)
+        self.cycle_system.cycleTimer = self.cycle_system.currentDuration()
+
+    def spawnEnemies(self, count: int) -> None:
+        target = pygame.Vector2(self.base.rect.center)
+        spawner = self.combat_system.enemySpawner
+        for _ in range(count):
+            enemyClass = random.choice(spawner.ENEMY_TYPES)
+            self.combat_system.enemies.append(enemyClass(spawner.getRandomEdgePosition(), target))
+
+    def maxResources(self) -> None:
+        self.rm.setStock(RessourceType.Energy, 9999)
+        self.rm.setStock(RessourceType.RawMaterial, 9999)
