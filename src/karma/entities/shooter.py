cf619924607b4118b0dab340d7ba67581a2573f0
NolanNtrg <notargiacomo.nolan07@gmail.com>
2026-09-17 22:15:17 +0200

    fix: baisser le son du piou piou zehma

--- src/karma/entities/shooter.py
+++ src/karma/entities/shooter.py
@@ -14,7 +14,7 @@ class Shooter:
     def __init__(self, bulletImage: pygame.Surface) -> None:
         if Shooter.bullet_sound is None:
                 Shooter.bullet_sound = pygame.mixer.Sound(SOUNDS_DIR / "gun-sound.mp3")
-                Shooter.bullet_sound.set_volume(0.1)
+                Shooter.bullet_sound.set_volume(0.05)
         self.bulletImage: pygame.Surface = bulletImage
         self.bullets: list[Bullet] = []
 
