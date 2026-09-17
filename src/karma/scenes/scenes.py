6b20216342bf9595ecb814b82ecf70b647ed8ec7
NolanNtrg <notargiacomo.nolan07@gmail.com>
2026-09-17 23:17:31 +0200

    feat: added ending sounds

--- src/karma/scenes/scenes.py
+++ src/karma/scenes/scenes.py
@@ -17,8 +17,12 @@ class Scene:
             MusicType.Menu: SOUNDS_DIR / "MenuMusic.mp3",
             MusicType.Day: SOUNDS_DIR / "DayMusic.mp3",
             MusicType.Night: SOUNDS_DIR / "NightMusic.mp3",
+            MusicType.GoodEnd: SOUNDS_DIR / "aura.mp3",
+            MusicType.BadEnd: SOUNDS_DIR / "seum.mp3",
+            MusicType.MidEnd: SOUNDS_DIR / "win.mp3",
         }
         path = music_files.get(music)
+
         pygame.mixer.music.load(path)
         pygame.mixer.music.play(-1)
         Scene.current_music = music
@@ -30,26 +34,31 @@ class Scene:
         elif game.state in (StateType.Play, StateType.Pause, StateType.Cinematic):
             track = MusicType.Day if game.cycle_system.isDay else MusicType.Night
             Scene.playMusic(track)
+        elif game.state == StateType.BadEnding:
+            Scene.playMusic(MusicType.BadEnd)
+        elif game.state == StateType.GoodEnding:
+            Scene.playMusic(MusicType.GoodEnd)
+        elif game.state == StateType.MidEnding:
+            Scene.playMusic(MusicType.MidEnd)
 
         if game.state == StateType.Play:
             PlayScene.updatePlayScene(game, dt)
         elif game.state == StateType.Pause and game.paused_from_explosion:
             game.update_explosion(dt)
-        elif game.state == StateType.Cinematic:
+        elif game.state == StateType.Cinematic or game.cinematic_player is not None:
             game.update_cinematic(dt)
 
     @staticmethod
     def drawScenes(game) -> None:
         # Rendu graphique
-        if game.state in (StateType.Menu, StateType.Credits, StateType.BadEnding, StateType.MidEnding, StateType.GoodEnding, StateType.HowToPlay):
-            MenuScene.drawMenuScene(game, game.state)
-        elif game.state == StateType.Cinematic:
+        if game.cinematic_player is not None and not (game.state == StateType.Pause and game.paused_from_explosion):
             game.cinematic_player.draw(game.screen)
+            if game.state == StateType.Pause and game.paused_from_cinematic:
+                game.pause_menu.draw(game.screen)
+        elif game.state in (StateType.Menu, StateType.Credits, StateType.BadEnding, StateType.MidEnding, StateType.GoodEnding, StateType.HowToPlay):
+            MenuScene.drawMenuScene(game, game.state)
         elif game.state == StateType.Pause and game.paused_from_explosion:
             PlayScene.drawPlayScene(game)
-        elif game.state == StateType.Pause and game.paused_from_cinematic:
-            game.cinematic_player.draw(game.screen)
-            game.pause_menu.draw(game.screen)
         else:
             PlayScene.drawPlayScene(game)
 
